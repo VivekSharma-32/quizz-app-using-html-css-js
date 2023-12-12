7fc9a19146fc937eef5fc804ae81f5a180822aa2
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-12-12 15:28:04 +0530

    changes

--- highScores.js
+++ highScores.js
@@ -1,7 +1,6 @@
 const highScoresList = document.getElementById("highScoreList");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
 highScoresList.innerHTML = highScores
   .map((score) => {
     return `<li class='high-score'>${score.name} - ${score.score}</li>`;
